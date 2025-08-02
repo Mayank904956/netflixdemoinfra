2bf7053190d6dc58c4b572ba691d636b05b85b72
Mayank904956 <mayankupadhyay304@gmail.com>
2025-08-03 02:04:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "ap-south-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0f918f7e67a3323f0"
-instance_type = "t2.medium"
-key_name = "RMUMBAIKP"
-vpc_security_group_ids = ["sg-0da91ed9997d755fc"]
+ami = "ami-000ec6c25978d5999"
+instance_type = "t2.micro"
+key_name = "sachin"
+vpc_security_group_ids = ["sg-04b9d7065bb851dd4"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
